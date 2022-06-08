91e078c407693cb9b48319ce8c8cf542aae6762c
evanalba <evanalba1403@gmail,com>
2022-06-07 21:39:36 -0700

    feat: added map to website.

---
+++ main.js
@@ -0,0 +1,20 @@
+import './style.css';
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import {fromLonLat} from 'ol/proj';
+
+const robinCenter = fromLonLat([-157.679559, 21.324921]); // longitude first, then latitude
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    center: robinCenter,
+    zoom: 17
+  })
+});
