ab354f6a2db2621c0280eca5649101703c514b77
evanalba <evanalba1403@gmail.com>
2022-06-10 19:03:01 -0700

    feat: Added background overlay protection for the credits pop-up.

--- js/base.js
+++ js/base.js
@@ -1,10 +1,12 @@
-$(document).ready(function() {
-    $('.credits').click(function() {
+$(document).ready(function () {
+    $('.credits').click(function () {
+        $('.credits-overlay').show()
         $('.credits-box').show();
         $(this).hide();
     });
 
-    $('.close-credits').click(function() {
+    $('.close-credits').click(function () {
+        $('.credits-overlay').hide();
         $('.credits-box').hide();
         $('.credits').show();
     });
