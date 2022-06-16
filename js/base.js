03cf5c74ff9551348f0fbe56c8e195bc29ddf98f
evanalba <evanalba1403@gmail.com>
2022-06-15 19:46:46 -0700

    feat: Added frontend for side panel.

--- js/base.js
+++ js/base.js
@@ -1,13 +1,34 @@
 $(document).ready(function () {
     $('.credits').click(function () {
-        $('.credits-overlay').show()
+        $('.credits-overlay').show();
         $('.credits-box').show();
+        // $('.credits-box').css('display')
         $(this).hide();
     });
 
     $('.close-credits').click(function () {
+        $('.credits').show();
         $('.credits-overlay').hide();
         $('.credits-box').hide();
-        $('.credits').show();
+    });
+
+    $('.side-panel-open').click(function () {
+        $('.side-panel-close').show();
+        $('.side-panel').hide();
+    });
+
+    $('.side-panel-close').click(function () {
+        $('.side-panel').show();
+        $(this).hide();
+    });
+
+    $('.side-panel-sliders').click(function () {
+        if ($('.legend-panel').is(':visible')) {
+            $('.place-panel').css('margin-top', '0em')
+            $('.legend-panel').hide();
+        } else {
+            $('.place-panel').css('margin-top', '6.25em')
+            $('.legend-panel').show();
+        }
     });
 });
