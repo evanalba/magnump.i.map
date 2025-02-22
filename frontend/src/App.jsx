833ca3e7dedb26062c681544a4eeffdefb1092c4
Evan Alba <evanalba@protonmail.com>
2025-02-22 02:48:01 -0800

    fix and feat: Minimal Vite + React setup.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import logo from "/logo.svg";
+import "./App.css";
+// import "./style.css";
+
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      {/* banner */}
+      {/* credits box pop-up  */}
+
+      {/* side panel */}
+    </>
+  );
+}
+
+export default App;
