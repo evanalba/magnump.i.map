f33f77224461d6bdad81370c278d104925ea7f83
Evan Alba <evanalba@protonmail.com>
2025-02-23 21:11:50 -0800

    feat: Docker compose on frontend working on port 3000.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from "@vitejs/plugin-react-swc";
+import dns from 'dns';
+
+// localhost part
+dns.setDefaultResultOrder('verbatim');
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 3000,
+  }
 })
