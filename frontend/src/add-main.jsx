3cb888257e7d0141f49864ba764c1cb22899ca98
Evan Alba <evanalba@protonmail.com>
2025-03-04 22:40:29 -0800

    fix: add.jsx access react bug fix.

--- frontend/src/add-main.jsx
+++ frontend/src/add-main.jsx
@@ -2,26 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Add from './add.jsx'
 
-// createRoot(document.getElementById('add-root')).render(
-//     <StrictMode>
-//       <Add />
-//     </StrictMode>,
-// )
-
-// if (rootElement) {
-//   createRoot(rootElement).render(<App />);
-// } else {
-//   console.log("Root element not found");
-// }
-
-const rootElement = document.getElementById('add-root'); // Define rootElement
-
-if (rootElement) {
-  createRoot(rootElement).render(
+createRoot(document.getElementById('add-root')).render(
     <StrictMode>
       <Add />
-    </StrictMode>
-  );
-} else {
-  console.log('Root element not found');
-}
+    </StrictMode>,
+)
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+// import react from '@vitejs/plugin-react';
+
 import dns from 'dns';
 import path from 'path';
 import fs from 'fs';
